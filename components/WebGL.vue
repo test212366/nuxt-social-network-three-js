c1d413faf26cbe641ccdbfeb88f59b0c9c8cf2fb
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 20:25:39 +0300

    Add files via upload

---
+++ components/WebGL.vue
@@ -0,0 +1,21 @@
+
+<script setup lang="ts">
+
+	import { WebGLScene } from '../class/WebGL'
+ 
+	const AimWebGL: Ref<HTMLCanvasElement | null> = ref(null)
+
+	onMounted(() => {
+		new WebGLScene({
+			dom: AimWebGL.value 
+		})
+	})
+ 
+	
+</script>
+
+<template> 
+	<div ref="AimWebGL">
+
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template> 
+	<!-- <h1>test</h1> -->
+	<WebGL />
+
+</template>
+
+
+<style  >
+
+	body {
+		overscroll-behavior: none;
+	}
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	 
+	canvas {
+		display: block;
+	}
+
+
+</style>
